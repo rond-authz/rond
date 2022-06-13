13f7a834feb3deb0a40d2a29c6486f7d57eee92d
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-06-13 22:36:50 +0200

    Timeout

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/rond-authz/rond/helpers"
 	"github.com/rond-authz/rond/internal/config"
@@ -95,8 +96,9 @@ func entrypoint(shutdown chan os.Signal) {
 	log.Trace("router setup completed")
 
 	srv := &http.Server{
-		Addr:    fmt.Sprintf("0.0.0.0:%s", env.HTTPPort),
-		Handler: router,
+		Addr:              fmt.Sprintf("0.0.0.0:%s", env.HTTPPort),
+		Handler:           router,
+		ReadHeaderTimeout: time.Millisecond * 5,
 	}
 
 	go func() {
