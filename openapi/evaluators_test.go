4c5b9af89ddccb1c125265230309859b9e0b991f
Davide Bianchi <bianchidavide12@gmail.com>
2023-07-13 19:07:25 +0200

    test: add more openapi tests

--- openapi/evaluators_test.go
+++ openapi/evaluators_test.go
@@ -52,4 +52,47 @@ func TestCreatePolicyEvaluators(t *testing.T) {
 		require.NoError(t, err, "unexpected error creating evaluators")
 		require.Len(t, policyEvals, 4, "unexpected length")
 	})
+
+	t.Run("with oas nil", func(t *testing.T) {
+		log, _ := test.NewNullLogger()
+		logger := logrus.NewEntry(log)
+		ctx := context.Background()
+
+		_, err := SetupEvaluators(ctx, logger, nil, nil, nil)
+		require.EqualError(t, err, "oas must not be nil")
+	})
+
+	t.Run("with complete oas mock", func(t *testing.T) {
+		log, _ := test.NewNullLogger()
+		logger := logrus.NewEntry(log)
+		ctx := context.Background()
+
+		opaModulesDirectory := "../mocks/rego-policies"
+
+		openApiSpec := &OpenAPISpec{
+			Paths: OpenAPIPaths{
+				"/invalid-path": PathVerbs{
+					"GET": VerbConfig{
+						PermissionV2: &core.RondConfig{},
+					},
+				},
+				"/path": PathVerbs{
+					"GET": VerbConfig{
+						PermissionV2: &core.RondConfig{
+							RequestFlow: core.RequestFlow{
+								PolicyName: "allow",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		opaModuleConfig, err := core.LoadRegoModule(opaModulesDirectory)
+		require.NoError(t, err, "unexpected error")
+
+		policyEvals, err := SetupEvaluators(ctx, logger, openApiSpec, opaModuleConfig, nil)
+		require.NoError(t, err, "unexpected error creating evaluators")
+		require.Len(t, policyEvals, 1, "unexpected length")
+	})
 }
